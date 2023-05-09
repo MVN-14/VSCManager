5086b5c3bc31f12577e85d445d39e4f8635ff271
MichaelVNogueira <m_nogueira82620@fanshaweonline.ca>
2023-05-09 15:09:54 -0400

    Implemented scrolling and refactored some classes
    Implemented scrolling and refactored some classes

---
+++ WorkspaceListPanel.cpp
@@ -0,0 +1,81 @@
+#include "WorkspaceListPanel.h"
+#include "WorkspaceListItem.h"
+#include "TextButton.h"
+#include "def.h"
+
+#include <wx/stattext.h>
+#include <wx/msgdlg.h>
+
+#include <fstream>
+#include <sstream>
+
+namespace {
+	wxString GetFolderNameFromPath(wxString const &path) {
+		int lastSlashPosition = path.Last('\\');
+
+		if (-1 != lastSlashPosition)
+			return path.SubString(static_cast<size_t>(lastSlashPosition) + 1, path.Length());
+		
+		return path;
+	}
+}
+
+WorkspaceListPanel::WorkspaceListPanel(wxWindow *parent) 
+	: wxScrolledWindow(parent) {
+	mainSizer_ = new wxBoxSizer(wxVERTICAL);
+	SetMinSize(wxSize(GetMinSize().x, 1000));
+	loadWorkspaces_();
+
+	SetScrollbars(1, 5, 50, 0);
+
+	//SetRowCount(mainSizer_->GetItemCount());
+	SetSizer(mainSizer_);
+}
+
+void WorkspaceListPanel::addPanelItem_(wxString const &workspaceName, wxString const &path) {
+	mainSizer_->Add(new WorkspaceListItem(this, workspaceName, path, [this, path] {
+		removePathFromFolderList_(path);
+		}), 0, wxEXPAND
+	);
+	GetParent()->Layout(); 
+}
+
+void WorkspaceListPanel::loadWorkspaces_() {
+	mainSizer_->Clear(true);
+	std::ifstream stream(FOLDER_LIST_PATH);
+
+	if (!stream) {
+		std::ofstream(FOLDER_LIST_PATH);
+		return;
+	}
+	
+	std::string path;
+	while (std::getline(stream, path)) {
+		if (!path.empty())
+			addPanelItem_(GetFolderNameFromPath(path), path);
+	}
+	GetParent()->Layout();
+}
+
+void WorkspaceListPanel::removePathFromFolderList_(wxString const &path) {
+	std::ifstream oldFile(FOLDER_LIST_PATH);
+	std::ofstream newFile("temp.txt");
+	if (!oldFile || !newFile)
+		return;
+
+	std::string line;
+	while (std::getline(oldFile, line)) 
+		if (line != path) 
+			newFile << line << '\n';
+
+	oldFile.close();
+	newFile.close();
+
+	remove(FOLDER_LIST_PATH);
+	rename("temp.txt", FOLDER_LIST_PATH);
+	loadWorkspaces_();
+}
+
+void WorkspaceListPanel::reloadWorkspaces() {
+	loadWorkspaces_();
+}
